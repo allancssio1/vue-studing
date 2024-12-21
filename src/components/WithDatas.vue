d899cd84335d17e6017566c6d5ebf06e7b8e2bf7
Allan Cassio <allan.cassio1@gmail.com>
2024-12-21 14:36:18 -0300

    implement coposition api to render data on screen

--- src/components/WithDatas.vue
+++ src/components/WithDatas.vue
@@ -19,14 +19,27 @@ p {
 }
 </style>
 
-<script>
-// optional API
-export default {
-  data: function () {
-    return {
-      productName: 'caderrno',
-      code: '#321',
-    }
-  },
-}
+<script setup>
+const productName = 'Borracha'
+const code = '#456'
 </script>
+<!-- <script>
+// optional API
+// export default {
+//   data: function () {
+//     return {
+//       productName: 'caderrno',
+//       code: '#321',
+//     }
+//   },
+// }
+
+// composition API
+// export default {
+//   setup: function () {
+//     const productName = 'lapis'
+//     const code = '#789'
+//     return { productName, code }
+//   },
+// }
+</script> -->
