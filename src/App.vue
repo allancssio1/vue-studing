442f180bc110d6909dd1ca9d3bb8f945040e6e68
Allan Cassio <allan.cassio1@gmail.com>
2024-12-21 15:49:23 -0300

    set to option api withDataNotPrimitive component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import FirstComponent from './components/FirstComponent.vue'
 import WithDatas from './components/WithDatas.vue'
 import WithDataPrimitive from './components/WithDataPrimitive.vue'
+import WithDataNotPrimitive from './components/WithDataNotPrimitive.vue'
 </script>
 
 <template>
   <header>Vue</header>
-  <FirstComponent />
-  <WithDatas />
+  <WithDataNotPrimitive />
   <WithDataPrimitive />
+  <WithDatas />
+  <FirstComponent />
 </template>
 
 <style scoped></style>
---
+++ src/components/WithDataNotPrimitive.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="component">
+    <h1>Component com dados reativos não primitivos</h1>
+    <p>Name: {{ people.name }} | Idade: {{ people.years }}</p>
+    <button @click="birthday">Aniversário</button>
+    <button @click="changeName()">Mudar Nome</button>
+  </div>
+</template>
+
+<!-- <script setup>
+import { reactive } from 'vue'
+
+const people = reactive({
+  name: 'Allan Cássio',
+  years: 33,
+})
+
+function birthday() {
+  people.years++
+}
+function changeName() {
+  people.name = 'Ferreia de Freitas'
+}
+</script> -->
+<script>
+export default {
+  data: function () {
+    return {
+      pessoa: {
+        name: 'Allan Cássio',
+        years: 33,
+      },
+    }
+  },
+  methods: {
+    birthday: function () {
+      this.pessoa.years++
+    },
+    changeName: function () {
+      this.pessoa.name = 'Ferreira de Freitas'
+    },
+  },
+}
+</script>
