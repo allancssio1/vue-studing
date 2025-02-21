2346476d5f7e68b00461e6cd0d04fc1ddaa1aed4
Allan Cassio <allan.cassio1@gmail.com>
2025-02-21 18:11:56 -0300

    continue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import FirstComponent from './components/FirstComponent.vue'
 import WithDatas from './components/WithDatas.vue'
 import WithDataPrimitive from './components/WithDataPrimitive.vue'
 import WithDataNotPrimitive from './components/WithDataNotPrimitive.vue'
+import DataBinding from './components/DataBinding.vue'
 </script>
 
 <template>
   <header>Vue</header>
+  <DataBinding />
   <WithDataNotPrimitive />
   <WithDataPrimitive />
   <WithDatas />
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="component">
+    <h1>Component com data binding</h1>
+    <input type="text" v-bind:value="valueInput" ></input>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const valueInput = ref('Aprendendo VueJs')
+</script>
